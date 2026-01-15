994cb0d05ad2ff5afeeac0823f0a6d87abc2c519
britty042 <brittney.law@gmail.com>
2026-01-15 11:17:07 -0700

    fixes to mission dark mode

--- mission/mission.js
+++ mission/mission.js
@@ -7,12 +7,10 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        document.body.style.backgroundColor = 'black';
-        document.body.style.color = 'white';
+        document.body.classList.add('dark');
         logo.src = 'byui-logo-white.png';
     } else {
-        document.body.style.backgroundColor = 'white';
-        document.body.style.color = 'black';
+        document.body.classList.remove('dark');
         logo.src = 'byui-logo-blue.webp';
     }
 }           
