a3d14ed7243ffafb188952ed6fce1b8ebf0be7fc
britty042 <brittney.law@gmail.com>
2026-01-15 11:04:44 -0700

    Mission dark mode completed

---
+++ mission/mission.js
@@ -0,0 +1,22 @@
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        logo.src = 'byui-logo-white.png';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        logo.src = 'byui-logo-blue.webp';
+    }
+}           
+                    
+
+//When dark mode is selected, background screen turns black, all but the blue letters turn white, and the image changes to white
+//when light mode is selected, or no mode chosen, screen in white, letters are black, and it includes the blue background image
